319c40497b69f4b949bee9872578d5140d1bae55
Alex <sashaoliyar7@gmail.com>
2024-06-26 09:47:44 +0300

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import NotFoundPage from "./pages/NotFound";
+import DashboardPage from "./pages/Dashboard";
+import ReviewPage from "./pages/Review";
+import LoginPage from "./pages/Login";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/review" element={<ReviewPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 
@@ -19,9 +20,11 @@ const darkTheme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
